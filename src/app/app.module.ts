1f02353679457a487d470bcea0b59017eec1f249
Razzakkhal <[khalfallah.razzak@gmail.com](mailto:khalfallah.razzak@gmail.com)>
2023-03-07 12:51:24 +0100

    creation of firsts components + add of pictures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { MyProfilComponent } from './my-profil/my-profil.component';
+import { GalerieComponent } from './galerie/galerie.component';
+import { OtherProfilComponent } from './other-profil/other-profil.component';
+import { TchatComponent } from './tchat/tchat.component';
+import { AdminInterfaceComponent } from './admin-interface/admin-interface.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    MyProfilComponent,
+    GalerieComponent,
+    OtherProfilComponent,
+    TchatComponent,
+    AdminInterfaceComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/other-profil/other-profil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other-profil',
+  templateUrl: './other-profil.component.html',
+  styleUrls: ['./other-profil.component.scss']
+})
+export class OtherProfilComponent {
+
+}
