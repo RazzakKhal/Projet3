449ea42eb5cbff3d416f9422a1e6d5953e6b2a3b
Razzakkhal <[khalfallah.razzak@gmail.com](mailto:khalfallah.razzak@gmail.com)>
2023-03-08 11:18:06 +0100

    add of User Class

---
+++ src/app/models/user.ts
@@ -0,0 +1,95 @@
+export class User {
+
+  private firstname : string;
+  private lastname : string;
+  private pseudo : string;
+  private date_Of_Birth : Date;
+  private mail : string;
+  private password : string;
+  private gender : string;
+
+
+    constructor(firstname : string, lastname:string, pseudo:string , date_Of_Birth:Date, mail:string, password:string, gender:string){
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.pseudo = pseudo;
+      this.date_Of_Birth = date_Of_Birth;
+      this.mail = mail;
+      this.password = password;
+      this.gender = gender;
+    }
+
+    sendLikes(){
+
+    }
+
+    getLikes(){
+
+    }
+
+    sendMessages(){
+
+    }
+
+    getMessages(){
+
+    }
+
+    getFirstname() : string{
+      return this.firstname;
+    }
+
+    setFirstname(firstname : string) : void {
+      this.firstname = firstname;
+    }
+
+    getLastname() : string{
+      return this.lastname;
+    }
+
+    setLastname(lastname : string) : void{
+      this.lastname = lastname;
+    }
+
+    getPseudo() : string{
+      return this.pseudo;
+    }
+
+    setPseudo(pseudo : string) : void{
+      this.pseudo = pseudo;
+    }
+
+    getDateOfBirth() : Date{
+      return this.date_Of_Birth;
+    }
+
+    setDateOfBirth(date_Of_Birth : Date){
+      this.date_Of_Birth = date_Of_Birth;
+    }
+
+    getMail() : string{
+      return this.mail;
+    }
+
+    setMail(mail : string){
+      this.mail = mail;
+    }
+
+    getPassword() : string{
+      return this.password;
+    }
+
+    setPassword(password : string){
+      this.password = password;
+    }
+
+    getGender() : string{
+      return this.gender;
+    }
+
+    setGender(gender : string){
+      this.gender = gender;
+    }
+
+
+}
