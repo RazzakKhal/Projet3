9d6e2ddf995e94b6f5f82d76b0dc0d07ae9da95c
Razzakkhal <[khalfallah.razzak@gmail.com](mailto:khalfallah.razzak@gmail.com)>
2023-03-17 11:40:55 +0100

    ajout classe picture

---
+++ src/app/models/picture.ts
@@ -0,0 +1,36 @@
+import { User } from './';
+
+export class Picture {
+  private id : number;
+  private link : string;
+  private user : User;
+
+  constructor(link : string, user: User){
+    this.link = link;
+    this.user = user;
+  }
+
+  public getId() : number {
+    return this.id;
+  }
+
+  public setId(id : number){
+    this.id = id;
+  }
+
+  public getLink() : string {
+    return this.link;
+  }
+
+  public setLink(link : string){
+    this.link = link;
+  }
+
+  public getUser() : User{
+    return this.user;
+  }
+
+  public setUser(user : User){
+    this.user = user;
+  }
+}
