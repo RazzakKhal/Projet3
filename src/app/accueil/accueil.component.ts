ad860019f30287cd87c4556b31f2a4b7e334a401
SandraConil <[sandra.conil@sncf.fr](mailto:sandra.conil@sncf.fr)>
2023-03-20 11:50:09 +0100

    modif home page

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../models/user';
 
 
 @Component({
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AccueilComponent {
 
-
+new User();
+fetch 
 }
