8c2ccead6464ed25ac80aab5bc85875610579dff
Gourmel <[eric.gourmel@sncf.fr](mailto:eric.gourmel@sncf.fr)>
2023-03-28 11:31:34 +0200

    adminShowUsers

--- src/app/admin-interface/admin-interface.component.ts
+++ src/app/admin-interface/admin-interface.component.ts
@@ -8,30 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AdminInterfaceComponent implements OnInit {
-ngOnInit(): void {
-  this.findAllUser()
-}
+  users: any[] = [];
+  userId: any;
+
+  ngOnInit(): void {
+   this.findAllUser()
+  }
+
 // faire une requete permettant de recuperer les utilisteurs
 findAllUser(){
 let token= localStorage.getItem("TokenSauvegarde");
+
 fetch("http://localhost:8080/admin/alluser",{
 method :"GET",
-headers: {"Content-Type": "application/json", "Authorization": "Bearer " + token }
-
-
-
+headers: {"Content-Type": "application/json",
+          "Authorization": "Bearer " + token }
   })
-
-
 
 .then((value) => value.json())
 .then((data) => {
-  
+  this.users = data
   console.log(data)
 })
 }
 // Afficher à l'aide d'une boucle tous les utilisateurs dans le HTML
-}
 // mettre un bouton pour supprimer l'utilisateur en question
 
+/*deleteUser() {
+  let token = localStorage.getItem("TokenSauvegarde");
+  fetch("http://localhost:8080/admin/deleteuser", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+  })
+}*/
 
+
+
+}
