2521b2caeab8f159b864c3221be1b28bd7e01c83
Razzakkhal <[khalfallah.razzak@gmail.com](mailto:khalfallah.razzak@gmail.com)>
2023-04-06 10:26:48 +0200

    add comment for next features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,13 +6,27 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'projet3SCSS';
 
   constructor(private router:Router){  }
   //   router est le nom de ma variable : type Router
   //router est une classe qui a pleins de methodes et proprieté dont l'url.
   // constructor (private router:Router) est une injection de dependence.
+
+
+ngOnInit(): void {
+  // toutes les minutes , SI LA PERSONNE EST CONNECTE, je récupère les likes. si il y en a un en plus, je les affiches
+// executé une methode toutes les minutes
+// cette methode verifie si la personne est connecté
+// si elle est connecté, requete pour récupérer les like
+// ces likes sont stocké dans une variables au dessus, si il y en a plus que la fois d'avant, notification reception like
+
+  
+}
+
+
+
 
   // methode pour cacher le header
 showNav(){
--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -16,16 +16,16 @@ export class MessageService {
   }
  
   initializeWebSocketConnection() {
-    // je configure la connexion avec le back afin de pouvoir echanger des messages via websocket
+    // je configure la connexion avec le endpoint du back afin de pouvoir echanger des messages via websocket
     const serverUrl = 'http://localhost:8080/chat';
-    const ws = new SockJS(serverUrl);
-    this.stompClient = Stomp.over(ws);
+    const socket  = new SockJS(serverUrl);
+    this.stompClient = Stomp.over(socket);
     const that = this;
     // tslint:disable-next-line:only-arrow-functions
     this.stompClient.connect({}, function(frame : any) {
       // je souscris à la route topic/messages, c'est cette route à laquelle le serveur renvoi le message
       that.stompClient.subscribe('/topic/messages', (message : any ) => {
- 
+  
       // on récupére le corps de la réponse
         if (message.body) {
           // le corps de la réponse est notre message sous forme d'objet JSON, on le parse et on l'envoi dans notre tableau de message
