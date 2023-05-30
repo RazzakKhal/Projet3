717d2539519c027b280e32bb54d06550e3ba038e
Razzakkhal <[khalfallah.razzak@gmail.com](mailto:khalfallah.razzak@gmail.com)>
2023-05-30 12:25:10 +0200

    guard like

--- src/app/guards/like.guard.ts
+++ src/app/guards/like.guard.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LikeGuard implements CanActivate {
 
-  constructor(private router: Router) { }
+  MyUser : any;
+
+  constructor(private router: Router, private authService : AuthService) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -15,10 +18,25 @@ export class LikeGuard implements CanActivate {
 
     //si mon utilisateur n'est pas connecté, il est orienté à la page d'accueil 
 
-    if (localStorage.getItem("TokenSauvegarde") === null) {
-      return this.router.navigate(['']);
-    }
-    return true;
-  }
+   // si il n'est pas connecté ça le renvoie à l'accueil
+   return this.authService.getUserConnected()
+   .then(reponse => reponse.json())
+   .then((data) => {
+     this.MyUser = data;
+     if (localStorage.getItem("TokenSauvegarde") === null) {
+       return this.router.navigate(['']);
+     }
+     // sinon si il est connecté mais il n'a pas de photo
+     // return this.router.navigate(['/myProfil']);
+     else if (this.MyUser.pictures.length === 0) {
+       return this.router.navigate(['/myProfil']);
+
+     } // sinon on retourne true;
+     return true;
+   }
+
+   )
+   .catch(() => false);
 }
 
+}
